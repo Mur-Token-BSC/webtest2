db8d71f3642bb4db228412ad8256360b1e81d2c1
Mur-Token-BSC <90121408+Mur-Token-BSC@users.noreply.github.com>
2021-09-10 00:02:22 +0530

    Add files via upload

---
+++ src/config/constants/farms.ts
@@ -0,0 +1,23 @@
+import { serializeTokens } from './tokens'
+import { SerializedFarmConfig } from './types'
+
+const serializedTokens = serializeTokens()
+
+const farms: SerializedFarmConfig[] = [
+  /**
+   * These 3 farms (PID 0, 251, 252) should always be at the top of the file.
+   */
+  {
+    pid: 0,
+    lpSymbol: 'CAKE',
+    lpAddresses: {
+      97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
+      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    },
+    token: serializedTokens.mur,
+    quoteToken: serializedTokens.busd,
+  },
+ 
+]
+
+export default farms
